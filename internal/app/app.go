c54167181ecd6723054074656ab31b47e9039059
vladimir.dumanovskiy <vladimir.dumanovskiy@iqoption.com>
2020-01-12 00:00:41 +0300

    Refactoring.

---
+++ internal/app/app.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"flag"
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+type App struct {
+}
+
+func NewApp() *App {
+	return &App{}
+}
+
+func (app *App) Run() {
+
+	secret := flag.String("secret", "", "pass secret to access api")
+	flag.Parse()
+	if secret == nil || *secret == "" {
+		log.Fatal("expected secret")
+	}
+
+	bot, err := tgbotapi.NewBotAPI(*secret)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message == nil { // ignore any non-Message Updates
+			continue
+		}
+
+		log.Printf("[%v %v] %v", update.Message.From.FirstName, update.Message.From.LastName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "What do you fill now?")
+
+		if _, er := bot.Send(msg); er != nil {
+			log.Println(er.Error())
+		}
+	}
+
+}
--- mood_inspector.go
+++ mood_inspector.go
@@ -1,45 +1,8 @@
 package main
 
-import (
-	"flag"
-	"log"
-
-	"github.com/go-telegram-bot-api/telegram-bot-api"
-)
+import "moodInspector/internal/app"
 
 func main() {
-
-	secret := flag.String("secret", "", "pass secret to access api")
-	flag.Parse()
-	if secret == nil || *secret == "" {
-		log.Fatal("expected secret")
-	}
-
-	bot, err := tgbotapi.NewBotAPI(*secret)
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bot.Debug = true
-
-	log.Printf("Authorized on account %s", bot.Self.UserName)
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 60
-
-	updates, err := bot.GetUpdatesChan(u)
-
-	for update := range updates {
-		if update.Message == nil { // ignore any non-Message Updates
-			continue
-		}
-
-		log.Printf("[%v %v] %v", update.Message.From.FirstName, update.Message.From.LastName, update.Message.Text)
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "What do you fill now?")
-
-		if _, er := bot.Send(msg); er != nil {
-			log.Println(er.Error())
-		}
-	}
+	application := app.NewApp()
+	application.Run()
 }
